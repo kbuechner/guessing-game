2a25e983f185f5eede83fef3f804d674239c6e43
Katherine Buechner <kbuechner@gmail.com>
2016-04-03 23:41:00 -0400

    some editing

--- js/guessing.js
+++ js/guessing.js
@@ -16,40 +16,50 @@ function getRandomInt(min, max) {
 function generateWinningNumber(){
 	// return a number between 1-100
 	// 	return Math.random();
-	playersGuess=0;
 	return getRandomInt(1, 100);
 }
+
 winningNumber = generateWinningNumber();
-console.log(winningNumber);
+playersGuess = 5;
+//	console.log(winningNumber);
 
 // Fetch the Players Guess
 
 function playersGuessSubmission(){
-	//if (submission NAN) do a 
+	--playersGuess;
+	//if submission > 100, not an int, or >100, text that says "you broke the rules and wasted a turn. was it worth it?" 
 	// add code here
-	return 2;
 }
 
 // Determine if the next guess should be a lower or higher number
 
 function lowerOrHigher(){
 
-	if( playerGuessSubmission() > winningNumber)
-		console.log("higher");
+	if( playerGuessSubmission() > winningNumber){
+	//	console.log("higher");
+	//	popup that says "nope! higher!";
+	}
+	else if (playerGuessSubmission() < winningNumber){
+	//	console.log("lower");
+	//	popup that says "nope! lower!";
+	}
 
-	else if (playerGuessSubmission() < winningNumber)
-		console.log("lower");
-
-	else if (playerGuessSubmission === winningNumber)
-		console.log("you win");
+	else if (playerGuessSubmission === winningNumber){}
+	//	console.log("you win");
+	// popup that says "you guessed it! you should put this on your resume."
 
 }
 
 // Check if the Player's Guess is the winning number 
 
 function checkGuess(){
-	// add code here
-}
+	if (playersGuess>0){
+		lowerOrHigher(guessInput);
+	}	// add code here
+	else{
+		//popup: sorry! try again? y/n
+	}
+
 
 // Create a provide hint button that provides additional clues to the "Player"
 
@@ -67,23 +77,25 @@ function provideHint(){
 		if (winningNumber < (100-hintRange)){
 			max = winningNumber + hintRange;
 		}
-		else
+		else{
 			max = 100;
-		console.log("The number is between " + getRandomInt(min, winningNumber) + " and "+ getRandomInt(winningNumber, max) + "! And the hint range is " + min + " to " + max + "!");
+		
+//		console.log("The number is between " + getRandomInt(min, winningNumber) + " and "+ getRandomInt(winningNumber, max) + "! And the hint range is " + min + " to " + max + "!");
 		--playersGuess;
+		}
 	}
-	else
-		//put in an "out of hints" dialogue
-		console.log("Out of hints, you clown.");
+	else{
+		//popup: you only get 5 hints");
+	}
 }
 
 // Allow the "Player" to Play Again
 
-function playAgain(){
+//function playAgain(){
 	// add code here
-}
-provideHint();
-provideHint();
-provideHint();
+//}
+//provideHint();
+//provideHint();
+//provideHint();
 
 /* **** Event Listeners/Handlers ****  */
